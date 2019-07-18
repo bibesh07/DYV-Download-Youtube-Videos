302125620cd5df1cd6d165b18def18f11205c5fd
Bibesh KC <bibesh.kc@selu.edu>
2019-07-17 23:24:29 -0500

    Added test modal

--- client/src/videoDescription/videoDescription.js
+++ client/src/videoDescription/videoDescription.js
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
+import { Button, Header, Icon, Modal, Card } from 'semantic-ui-react'
 
 export function VideoDescription() {
-    const[videoUrl, setVideoUrl] = useState('test');
+    const[isOpen, setVideoUrl] = useState('true');
+    
     return (
-        <div>
-            <iframe id="ytplayer" type="text/html" width="340" height="250" src="https://www.youtube.com/embed/S1edY5Plsu0" frameborder="0"></iframe>
-        </div>
+        <Modal open={isOpen} size='tiny' closeOnEscape={true} closeOnDimmerClick={false}>
+            <Modal.Header>Is this the video you are looking to download?</Modal.Header>
+            <Modal.Content>
+                <iframe id="ytplayer" type="text/html"  width="470" height="300" src="https://www.youtube.com/embed/S1edY5Plsu0" frameborder="0"></iframe>
+                <hr/>
+                <Modal.Description>
+                    <Header>Default Profile Image</Header>
+                    <p>We've found the following gravatar image associated with your e-mail address.</p>
+                    <p>Is it okay to use this photo?</p>
+                </Modal.Description>
+            </Modal.Content>
+        </Modal>
     )
 }
